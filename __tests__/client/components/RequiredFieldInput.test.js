0363d51ae88ffee8a77bc2f4996ceb7ae64d55b7
Anna Mai <annam0403@gmail.com>
2019-08-28 16:02:17 -0400

    Add test for static form field component

---
+++ __tests__/client/components/RequiredFieldInput.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Input } from 'semantic-ui-react';
+import '@testing-library/jest-dom/extend-expect';
+import { RequiredFieldInput } from '../../../client/components';
+
+describe('RequiredFieldInput', () => {
+  it('should render a form field based on an input object', () => {
+    const field = {
+      name: 'Field',
+      placeholder: 'Field',
+      type: 'text'
+    };
+    const inputs = {
+      field: 'fieldValue'
+    };
+    const control = Input;
+    const onChange = jest.fn();
+    const { container } = render(
+      <RequiredFieldInput
+        field={field}
+        inputs={inputs}
+        control={control}
+        onChange={onChange}
+      />
+    );
+
+    expect(
+      container.querySelector(`input#${field.name.toLowerCase()}`).value
+    ).toBe('fieldValue');
+  });
+});
--- client/components/RequiredFieldInput.jsx
+++ client/components/RequiredFieldInput.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Form } from 'semantic-ui-react';
 
-const RequiredFieldInput = props => {
-  const { field, inputs, control, onChange } = props;
+const RequiredFieldInput = ({ field, inputs, control, onChange }) => {
   const { name, placeholder, type } = field;
   const nameLower = name.toLowerCase();
   return (
